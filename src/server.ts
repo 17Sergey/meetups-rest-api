279b350580b3491eea2de663ffae729fcd7a5f08
Sergey Kachaliuk <sergeykachaliukdev@gmail.com>
2025-02-20 16:02:00 +0300

    setup: express server added

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express + TypeScript Server");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
