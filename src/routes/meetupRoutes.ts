7a49e578bdb0abc1be9e5bf30d9193c0cba7df50
Sergey Kachaliuk <sergeykachaliukdev@gmail.com>
2025-02-26 21:28:21 +0300

    feat: add swagger docs to routes file

--- src/routes/meetupRoutes.ts
+++ src/routes/meetupRoutes.ts
@@ -1,21 +1,39 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import {
-  createMeetup,
-  deleteMeetup,
-  getAllMeetups,
-  getMeetupById,
-  updateMeetup,
-} from "@controllers/meetupControllers";
+import { getAllMeetups } from "@controllers/meetup/getAllMeetups";
+import { getMeetupById } from "@controllers/meetup/getMeetupById";
+import { createMeetup } from "@controllers/meetup/createMeetup";
+import { updateMeetup } from "@controllers/meetup/updateMeetup";
+import { deleteMeetup } from "@controllers/meetup/deleteMeetup";
+import { validateIdParameter } from "@middleware/validateIdParameter.js";
+import { numericIdSchema } from "@utils/dto/numericId.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Meetups
+ *   description: API for managing meetups
+ */
+
 router.get("/", getAllMeetups);
-router.get("/:id", getMeetupById);
+router.get("/:id", validateIdParameter(numericIdSchema), getMeetupById);
 
 router.post("/", protectRoute, createMeetup);
 
-router.put("/:id", protectRoute, updateMeetup);
-router.delete("/:id", protectRoute, deleteMeetup);
+router.put(
+  "/:id",
+  protectRoute,
+  validateIdParameter(numericIdSchema),
+  updateMeetup,
+);
+
+router.delete(
+  "/:id",
+  protectRoute,
+  validateIdParameter(numericIdSchema),
+  deleteMeetup,
+);
 
 export default router;
