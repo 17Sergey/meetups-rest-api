68d71ee4fedd54f7d2d1ec52769400e11833e22b
Sergey Kachaliuk <sergeykachaliukdev@gmail.com>
2025-02-27 16:24:26 +0300

    feat: password helpers

---
+++ src/utils/passwordHelpers.ts
@@ -0,0 +1,15 @@
+import bcrypt from "bcrypt";
+
+const SALT_ROUNDS = 10;
+
+export const passwordHelpers = {
+  async hashPassword(password: string) {
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    const passwordHash = await bcrypt.hash(password, salt);
+    return passwordHash;
+  },
+
+  async comparePassword(password: string, hashedPassword: string) {
+    return await bcrypt.compare(password, hashedPassword);
+  },
+};
