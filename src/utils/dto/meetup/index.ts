cbe6c66e90a20170d101943d6659d77735f2eaf2
Sergey Kachaliuk <sergeykachaliukdev@gmail.com>
2025-03-03 13:47:11 +0300

    feat: add validation messages about required fields

--- src/utils/dto/meetup/index.ts
+++ src/utils/dto/meetup/index.ts
@@ -2,26 +2,22 @@ import { z } from "zod";
 
 export const createMeetupSchema = z.object({
   title: z
-    .string()
-    .max(100, { message: "Title must be at most 100 characters long." })
-    .nonempty({ message: "Title is required." }),
+    .string({ message: "Title is required." })
+    .max(100, { message: "Title must be at most 100 characters long." }),
 
   description: z
-    .string()
-    .max(500, { message: "Description must be at most 500 characters long." })
-    .nonempty({ message: "Description is required." }),
+    .string({ message: "Description is required." })
+    .max(500, { message: "Description must be at most 500 characters long." }),
 
   dateTime: z
-    .string()
-    .nonempty({ message: "Date and time are required." })
+    .string({ message: "Datetime is required." })
     .refine((date) => !isNaN(Date.parse(date)), {
       message: "Invalid date format. Please use a valid ISO date format.",
     }),
 
   location: z
-    .string()
-    .max(200, { message: "Location must be at most 200 characters long." })
-    .nonempty({ message: "Location is required." }),
+    .string({ message: "Location is required." })
+    .max(200, { message: "Location must be at most 200 characters long." }),
 });
 
 export const updateMeetupSchema = z.object({
--- src/utils/dto/user/index.ts
+++ src/utils/dto/user/index.ts
@@ -1,15 +1,18 @@
+import { USER_ROLES } from "@utils/constants";
 import z from "zod";
 
 export const signupSchema = z.object({
   fullName: z
-    .string()
+    .string({ message: "Full name is required" })
     .min(2, { message: "Full name must be at least 2 characters long." })
     .max(50, { message: "Full name must be maximum 50 characters long." }),
 
-  email: z.string().email({ message: "Invalid email address." }),
+  email: z
+    .string({ message: "Email is required" })
+    .email({ message: "Invalid email address." }),
 
   password: z
-    .string()
+    .string({ message: "Password is required" })
     .min(6, { message: "Password must be at least 6 characters long." })
     .max(20, { message: "Password must be maximum 20 characters long." })
     .regex(/[a-z]/, {
@@ -22,19 +25,33 @@ export const signupSchema = z.object({
     .regex(/[\W_]/, {
       message: "Password must contain at least one special character.",
     }),
+
+  role: z.enum([USER_ROLES.PARTICIPANT, USER_ROLES.ORGANIZER], {
+    message:
+      "Role is required and must be either 'participant' or 'organizer'.",
+  }),
 });
 
 export const loginSchema = z.object({
-  email: z.string().email({ message: "Invalid email address." }),
+  email: z
+    .string({ message: "Email is required" })
+    .email({ message: "Invalid email address." })
+    .refine((value) => Boolean(value), {
+      message: "Email is required.",
+    }),
 
   password: z
-    .string()
-    .min(6, { message: "Password must be at least 6 characters long." }),
+    .string({ message: "Password is required" })
+    .min(6, { message: "Password must be at least 6 characters long." })
+    .refine((value) => Boolean(value), {
+      message: "Password is required.",
+    }),
 });
 
 export const refreshSchema = z.object({
-  refreshToken: z.string(),
+  refreshToken: z.string({ message: "Refresh token is required" }),
 });
 
 export type SignupSchema = z.infer<typeof signupSchema>;
 export type LoginSchema = z.infer<typeof loginSchema>;
+export type RefreshSchema = z.infer<typeof refreshSchema>;
