ee8a1867ce4da7a522f821550e6f4e88c847193b
Sergey Kachaliuk <sergeykachaliukdev@gmail.com>
2025-03-03 13:51:09 +0300

    fix: add check for stored token when trying to logout

--- src/services/user/logout.ts
+++ src/services/user/logout.ts
@@ -3,6 +3,14 @@ import { errorHeplers } from "@utils/errors/errorHelpers";
 
 export const logout = async (userId: number): Promise<ServiceResult> => {
   try {
+    const storedToken = await refreshTokenRepository.getByUserId(userId);
+    if (!storedToken) {
+      return {
+        statusCode: 400,
+        jsonResponse: { message: "Logout failed: user already logged out" },
+      };
+    }
+
     await refreshTokenRepository.deleteByUserId(userId);
 
     return {
